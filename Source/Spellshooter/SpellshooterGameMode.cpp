9544cb18f69403219d1a74f77d2b53c96b4e3c90
Vegardoa <voaarnes@gmail.com>
2020-11-05 04:07:04 +0100

    Camera- and Animation fixes

--- Source/Spellshooter/SpellshooterGameMode.cpp
+++ Source/Spellshooter/SpellshooterGameMode.cpp
@@ -1,15 +1,28 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "SpellshooterGameMode.h"
-#include "SpellshooterCharacter.h"
+#include "humanCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ASpellshooterGameMode::ASpellshooterGameMode()
 {
+	UWorld* World = GetWorld();
+	//if (!ensure(World != nullptr)) return;
+
+	//for (FConstPlayerControllerIterator PCIterator = World->GetPlayerControllerIterator(); PCIterator; PCIterator++) {
+
+	//	APlayerController* playerController = PCIterator->Get();
+	//	AHumanCharacter* PlayerPawn = Cast<AHumanCharacter>(playerController->GetPawn());
+
+
+
+	//}
+
+	DefaultPawnClass = AHumanCharacter::StaticClass();
+
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Human/Blueprints/humanCharacterBP"));
+	if (PlayerPawnBPClass.Class != NULL) {
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 }
