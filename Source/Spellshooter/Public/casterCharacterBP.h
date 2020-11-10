ebba45be3bf02bcf9a341844a880a08d080b5f64
iaminadequate <theocg@stud.ntnu.no>
2020-11-10 13:19:28 +0100

    added second class, ability to switch between shooter and caster using f

---
+++ Source/Spellshooter/Public/casterCharacterBP.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "casterCharacterBP.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UStaticMeshComponent; //might be obsolete
+
+
+UCLASS()
+class SPELLSHOOTER_API AcasterCharacterBP : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AcasterCharacterBP();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+		USpringArmComponent* SpringArmComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera")
+		UCameraComponent* CameraComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player")   //might be obsolete
+		UStaticMeshComponent* MeshComp;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void TurnAtRate(float Value);
+	void LookUpAtRate(float Value);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+		float BaseTurnRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+		float BaseLookUpAtRate;
+		
+
+public:	
+	
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
